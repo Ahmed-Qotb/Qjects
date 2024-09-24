2fe9377fd386cf3bfe4cd69a0a47077d53cb6853
ahmed <ahmedhassan99fg@gmail.com>
2024-09-24 14:11:28 +0300

    footer done

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,14 +1,76 @@
 import Image from "next/image";
 import logo from "@/assets/images/Monogram-QJ-Logo-Design-by-Greenlines-Studios-removebg-preview.png";
+import { SiLinkedin } from "@icons-pack/react-simple-icons";
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import { SiGmail } from "@icons-pack/react-simple-icons";
+import Link from "next/link";
 
 function footer() {
+  // ? array of nav links
+  const navLinks: { href: string; label: string }[] = [
+    {
+      href: "/skills",
+      label: "Skills",
+    },
+    {
+      href: "/projects",
+      label: "Projects",
+    },
+    {
+      href: "/contact",
+      label: "Contact",
+    },
+  ];
+
   return (
     <footer className="main-color-dark-bg py-5">
-      <div className="container flex justify-center items-center sm:gap-5 flex-wrap sm:flex-nowrap">
+      <div className="container flex justify-center sm:gap-5 flex-wrap sm:flex-nowrap">
         {/* socials */}
-        <div className="w-[33.33%]">1</div>
+        <div className="w-[50%] text-center">
+          <h3 className="font-extrabold text-white text-xl">Social Links</h3>
+          <ul className="flex justify-center items-center gap-4 py-5">
+            {/* linked in */}
+            <li>
+              <Link href={`https://www.linkedin.com/in/ahmed-qotb-043850296/`}>
+                <SiLinkedin className="bg-black " color="white" />
+              </Link>
+            </li>
+            {/* github */}
+            <li>
+              <Link href={`https://github.com/Ahmed-Qotb`}>
+                <SiGithub
+                  className="bg-black hover:text-[#FF7517]"
+                  color="white"
+                />
+              </Link>
+            </li>
+            {/* gmail email to */}
+            <li>
+              <Link href={"mailto:ahmedhassan99fg@gmail.com"}>
+                <SiGmail className="bg-black" color="white" />
+              </Link>
+            </li>
+          </ul>
+        </div>
         {/* important links */}
-        <div className="w-[33.33%]">2</div>
+        <div className="w-[50%]  text-center">
+          <h3 className="font-extrabold text-white text-xl">Quick Links</h3>
+          <ul className="flex flex-col items-center">
+          {navLinks.map(
+              (link: { href: string; label: string }, index: number) => (
+                <li className="">
+                  <Link
+                    key={index}
+                    href={link.href}
+                    className="hover:text-[#FF7517]"
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
         {/* logo with description */}
         <div className="w-[100%] ">
           <Image
